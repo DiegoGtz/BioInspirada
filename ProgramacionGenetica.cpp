35d122d3a0818290f176ef43ae19957f3be265de
Diego[GTZ] <dartz160@gmail.com>
2021-11-16 18:36:04 -0500

    Variables

--- ProgramacionGenetica.cpp
+++ ProgramacionGenetica.cpp
@@ -1,3 +1,54 @@
-#include <iostream>
+//Practica 04 Bioinspirada CS-UNSA
+// DiegoGTZ - Brayan 
 
-using namespacae std;
+
+//Encontrar la función matemática que se ajuste al conjunto de 10 ejemplos:
+
+// Entrada - Salida  
+// 0 -  0
+// 0.1 - 0.005
+// 0.2 - 0.02
+// 0.3 - 0.045 
+// 0.4 - 0.08
+// 0.5 - 0.125
+// 0.6 - 0.18
+// 0.7 - 0.245 
+// 0.8 - 0.32 
+// 0.9 - 0.405
+// Variable :  X 
+// Terminales : -5 .... + 5 
+// Conjunto de funciones  : +, - , *, %, /. 
+// Error cuadratico medio 
+// utiliza reproduccion  y mutacion 
+
+
+
+
+#include<iostream>
+
+using namespace std; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    
+    cout<< "Practica de Programacion Geneica"  ; 
+    cout<<"asd";
+
+
+    return 0;
+}
+
+
